7c4efc52f56947493d982434b1674a8813d67684
BixGra <bixente.grandjean@gmail.com>
2024-11-30 17:46:03 +0100

    [+] Infra rework

---
+++ start.sh
@@ -0,0 +1,11 @@
+echo "docker build . -t randomweather"
+
+docker build . -t randomweather
+
+echo "docker compose up -d --remove-orphans"
+
+docker compose up -d --remove-orphans
+
+echo "docker image prune -a"
+
+docker image prune -a
